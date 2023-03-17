016809f0234b6da5758ab30a70a0727b08fe8822
Sarah Simorangkir <sarahsimorangkir34@gmail.com>
2023-03-17 11:33:42 +0700

    Done the first challenge (easy level)

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Barcode from "./assets/image-qr-code.png";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Card">
+        <img src={Barcode} alt="" />
+        <div className="CardDetails">
+          <p className="Caption">
+            Improve your frontend skills by building projects
+          </p>
+          <p className="CapDetails">
+            Scan the QR Code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
